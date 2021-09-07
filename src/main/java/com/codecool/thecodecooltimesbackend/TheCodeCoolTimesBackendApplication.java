506998ef61c2e23b001b9c3c20633766a3f97279
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-09-07 09:53:16 +0200

    [add] starter project

---
+++ src/main/java/com/codecool/thecodecooltimesbackend/TheCodeCoolTimesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.thecodecooltimesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCodeCoolTimesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheCodeCoolTimesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/thecodecooltimesbackend/TheCodeCoolTimesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.thecodecooltimesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCodeCoolTimesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
