cd58034df6cdf0b404aaccf490c1922d56336324
Zsolt Rokolya <rokyhu@gmail.com>
2021-09-07 15:43:49 +0200

    [update] Source model

---
+++ src/main/java/com/codecool/thecodecooltimesbackend/model/Source.java
@@ -0,0 +1,15 @@
+package com.codecool.thecodecooltimesbackend.model;
+
+public class Source {
+
+    private String id;
+    private String name;
+
+    public Source(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Source() {
+    }
+}
