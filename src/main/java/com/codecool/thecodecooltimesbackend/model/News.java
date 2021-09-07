19615e3830c480c099c6f7e64325ba737e72b260
Zsolt Rokolya <rokyhu@gmail.com>
2021-09-07 15:43:37 +0200

    [update] News model

--- src/main/java/com/codecool/thecodecooltimesbackend/model/News.java
+++ src/main/java/com/codecool/thecodecooltimesbackend/model/News.java
@@ -1,5 +1,29 @@
 package com.codecool.thecodecooltimesbackend.model;
 
-public class News {
+import java.io.Serializable;
 
+public class News implements Serializable {
+
+    private Source source;
+    private String author;
+    private String title;
+    private String description;
+    private String url;
+    private String urlToImage;
+    private String publishedAt;
+    private String content;
+
+    public News(Source source, String author, String title, String description, String url, String urlToImage, String publishedAt, String content) {
+        this.source = source;
+        this.author = author;
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.urlToImage = urlToImage;
+        this.publishedAt = publishedAt;
+        this.content = content;
+    }
+
+    public News() {
+    }
 }
