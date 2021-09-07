22e3f51888a748b531412108ef1c2a63496a104e
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-09-07 16:30:02 +0200

    [add] getters and setters to NewsResults

--- src/main/java/com/codecool/thecodecooltimesbackend/model/NewsResults.java
+++ src/main/java/com/codecool/thecodecooltimesbackend/model/NewsResults.java
@@ -1,26 +1,26 @@
 package com.codecool.thecodecooltimesbackend.model;
 
-import java.io.Serializable;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class NewsResults implements Serializable {
-    private String status;
-    private int totalResults;
+public class NewsResults {
     private List<News> articles = new ArrayList<>();
-
-    public NewsResults(String status, int totalResults, List<News> articles) {
-        this.status = status;
-        this.totalResults = totalResults;
-        this.articles = articles;
-    }
 
     public NewsResults() {
     }
 
+    public List<News> getArticles() {
+        return articles;
+    }
+
+    public void setArticles(List<News> articles) {
+        this.articles = articles;
+    }
+
     @Override
     public String toString() {
-        return "NewsResults{ Status: " + status + "}";
+        return "NewsResults{ " + articles + "}";
     }
 }
 
