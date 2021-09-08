7beabca815fb616749cb7c5da417840ae007a371
Zsolt Rokolya <rokyhu@gmail.com>
2021-09-08 14:56:43 +0200

    [add] Quote model

---
+++ src/main/java/com/codecool/thecodecooltimesbackend/model/quote/Quote.java
@@ -0,0 +1,22 @@
+package com.codecool.thecodecooltimesbackend.model.quote;
+
+public class Quote {
+
+    private String quote;
+
+    public Quote(String quote) {
+        this.quote = quote;
+    }
+
+    public Quote() {
+
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+}
