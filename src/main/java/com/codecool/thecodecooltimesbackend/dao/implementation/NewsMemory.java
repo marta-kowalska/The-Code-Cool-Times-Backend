ec8e2be1d034c294faa1aea59bfc31f2edaa5338
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-09-08 11:45:25 +0200

    [change] NewsMemory to use ApiRequester

--- src/main/java/com/codecool/thecodecooltimesbackend/dao/implementation/NewsMemory.java
+++ src/main/java/com/codecool/thecodecooltimesbackend/dao/implementation/NewsMemory.java
@@ -1,16 +1,20 @@
 package com.codecool.thecodecooltimesbackend.dao.implementation;
 
 import com.codecool.thecodecooltimesbackend.dao.NewsDAO;
-import com.codecool.thecodecooltimesbackend.model.News;
+import com.codecool.thecodecooltimesbackend.model.NewsResults;
+import com.codecool.thecodecooltimesbackend.util.ApiRequester;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository("newsMemory")
 public class NewsMemory implements NewsDAO {
 
+
+    private static final String API_URL = "https://newsapi.org/v2/everything?qInTitle=everything&pageSize=100&apiKey=803b1f20229542109d3b21b58d162064";
+
     @Override
-    public List<News> getTopNews() {
-        return null;
+    public NewsResults getTopNews() {
+        return ApiRequester.fetchData(API_URL, NewsResults.class);
+
     }
 }
