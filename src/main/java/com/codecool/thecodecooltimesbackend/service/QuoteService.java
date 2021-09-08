ab9e587c447e957f4e336deb70d8f97b154c2484
Zsolt Rokolya <rokyhu@gmail.com>
2021-09-08 18:08:13 +0200

    [refactor] remove unused imports

--- src/main/java/com/codecool/thecodecooltimesbackend/service/QuoteService.java
+++ src/main/java/com/codecool/thecodecooltimesbackend/service/QuoteService.java
@@ -1,11 +1,7 @@
 package com.codecool.thecodecooltimesbackend.service;
 
-
-import com.codecool.thecodecooltimesbackend.config.AppProperties;
-import com.codecool.thecodecooltimesbackend.dao.NewsDAO;
 import com.codecool.thecodecooltimesbackend.dao.quoteDAO.QuoteDAO;
 import com.codecool.thecodecooltimesbackend.model.quote.Quote;
-import com.codecool.thecodecooltimesbackend.util.ApiRequester;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
