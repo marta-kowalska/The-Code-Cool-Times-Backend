dccda7a3a89d526ed47a312684d4de6f3618256e
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-09-08 11:46:17 +0200

    [refactor] NewsService to use newsDAO

--- src/main/java/com/codecool/thecodecooltimesbackend/service/NewsService.java
+++ src/main/java/com/codecool/thecodecooltimesbackend/service/NewsService.java
@@ -1,20 +1,15 @@
 package com.codecool.thecodecooltimesbackend.service;
 
 import com.codecool.thecodecooltimesbackend.dao.NewsDAO;
-import com.codecool.thecodecooltimesbackend.model.News;
 import com.codecool.thecodecooltimesbackend.model.NewsResults;
-import com.codecool.thecodecooltimesbackend.util.ApiRequester;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 
-import java.util.List;
-
 @Component
 public class NewsService {
     private final NewsDAO newsDAO;
-    private static final String API_URL = "https://newsapi.org/v2/everything?qInTitle=everything&pageSize=100&apiKey=803b1f20229542109d3b21b58d162064";
 
     @Autowired
     public NewsService(@Qualifier("newsMemory") NewsDAO newsDAO) {
@@ -22,10 +17,10 @@ public class NewsService {
     }
 
     public NewsResults getTopNews(){
-        return ApiRequester.fetchData(API_URL, NewsResults.class);
+        return newsDAO.getTopNews();
     }
 
-    public List<News> getNewsForCategory(String category){
+    public NewsResults getNewsForCategory(String category){
         return null;
     }
 
