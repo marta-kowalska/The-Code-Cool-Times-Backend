730e6fa90d094c076c1cafdeb34af5fe84bd38c6
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-09-08 14:32:00 +0200

    [add] ingredients and measures and refactor

--- src/main/java/com/codecool/thecodecooltimesbackend/model/recipe/Recipe.java
+++ src/main/java/com/codecool/thecodecooltimesbackend/model/recipe/Recipe.java
@@ -1,32 +1,26 @@
 package com.codecool.thecodecooltimesbackend.model.recipe;
 
+import java.util.*;
+
 public class Recipe {
     private String strMeal;
     private String strInstructions;
     private String strMealThumb;
-    private String strIngredient1;
-    private String strIngredient2;
-    private String strIngredient3;
-    private String strIngredient4;
-    private String strIngredient5;
-    private String strIngredient6;
-    private String strIngredient7;
-    private String strIngredient8;
-    private String strIngredient9;
-    private String strIngredient10;
-    private String strMeasure1;
-    private String strMeasure2;
-    private String strMeasure3;
-    private String strMeasure4;
-    private String strMeasure5;
-    private String strMeasure6;
-    private String strMeasure7;
-    private String strMeasure8;
-    private String strMeasure9;
-    private String strMeasure10;
     private String strSource;
 
     public Recipe() {
+    }
+
+    public List<String> ingredients = new ArrayList<>();
+    public List<String> measures = new ArrayList<>();
+
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public void setMeasurements(List<String> measures) {
+        this.measures = measures;
     }
 
     public String getStrSource() {
@@ -35,6 +29,18 @@ public class Recipe {
 
     public void setStrSource(String strSource) {
         this.strSource = strSource;
+    }
+
+    private void addIngredient(String ingredient){
+        if(!ingredient.equals("") && !ingredient.equals(" ") ){
+            ingredients.add(ingredient);
+        }
+    }
+
+    private void addMeasure(String measure){
+        if(!measure.equals("") && !measure.equals(" ") ){
+            measures.add(measure);
+        }
     }
 
 
@@ -62,163 +68,88 @@ public class Recipe {
         this.strMealThumb = strMealThumb;
     }
 
-    public String getStrIngredient1() {
-        return strIngredient1;
-    }
 
     public void setStrIngredient1(String strIngredient1) {
-        this.strIngredient1 = strIngredient1;
-    }
-
-    public String getStrIngredient2() {
-        return strIngredient2;
+        addIngredient(strIngredient1);
     }
 
     public void setStrIngredient2(String strIngredient2) {
-        this.strIngredient2 = strIngredient2;
-    }
-
-    public String getStrIngredient3() {
-        return strIngredient3;
+        addIngredient(strIngredient2);
     }
 
     public void setStrIngredient3(String strIngredient3) {
-        this.strIngredient3 = strIngredient3;
-    }
-
-    public String getStrIngredient4() {
-        return strIngredient4;
+        addIngredient(strIngredient3);
     }
 
     public void setStrIngredient4(String strIngredient4) {
-        this.strIngredient4 = strIngredient4;
-    }
-
-    public String getStrIngredient5() {
-        return strIngredient5;
+        addIngredient(strIngredient4);
     }
 
     public void setStrIngredient5(String strIngredient5) {
-        this.strIngredient5 = strIngredient5;
-    }
-
-    public String getStrIngredient6() {
-        return strIngredient6;
+        addIngredient(strIngredient5);
     }
 
     public void setStrIngredient6(String strIngredient6) {
-        this.strIngredient6 = strIngredient6;
-    }
-
-    public String getStrIngredient7() {
-        return strIngredient7;
+        addIngredient(strIngredient6);
     }
 
     public void setStrIngredient7(String strIngredient7) {
-        this.strIngredient7 = strIngredient7;
-    }
-
-    public String getStrIngredient8() {
-        return strIngredient8;
+        addIngredient(strIngredient7);
     }
 
     public void setStrIngredient8(String strIngredient8) {
-        this.strIngredient8 = strIngredient8;
+        addIngredient(strIngredient8);
     }
 
-    public String getStrIngredient9() {
-        return strIngredient9;
-    }
 
     public void setStrIngredient9(String strIngredient9) {
-        this.strIngredient9 = strIngredient9;
+        addIngredient(strIngredient9);
     }
 
-    public String getStrIngredient10() {
-        return strIngredient10;
-    }
 
     public void setStrIngredient10(String strIngredient10) {
-        this.strIngredient10 = strIngredient10;
+        addIngredient(strIngredient10);
     }
 
-    public String getStrMeasure1() {
-        return strMeasure1;
-    }
 
     public void setStrMeasure1(String strMeasure1) {
-        this.strMeasure1 = strMeasure1;
-    }
-
-    public String getStrMeasure2() {
-        return strMeasure2;
+        addMeasure(strMeasure1);
     }
 
     public void setStrMeasure2(String strMeasure2) {
-        this.strMeasure2 = strMeasure2;
+        addMeasure(strMeasure2);
     }
 
-    public String getStrMeasure3() {
-        return strMeasure3;
+   public void setStrMeasure3(String strMeasure3) {
+       addMeasure(strMeasure3);
     }
 
-    public void setStrMeasure3(String strMeasure3) {
-        this.strMeasure3 = strMeasure3;
-    }
-
-    public String getStrMeasure4() {
-        return strMeasure4;
-    }
 
     public void setStrMeasure4(String strMeasure4) {
-        this.strMeasure4 = strMeasure4;
-    }
-
-    public String getStrMeasure5() {
-        return strMeasure5;
+        addMeasure(strMeasure4);
     }
 
     public void setStrMeasure5(String strMeasure5) {
-        this.strMeasure5 = strMeasure5;
-    }
-
-    public String getStrMeasure6() {
-        return strMeasure6;
+        addMeasure(strMeasure5);
     }
 
     public void setStrMeasure6(String strMeasure6) {
-        this.strMeasure6 = strMeasure6;
-    }
-
-    public String getStrMeasure7() {
-        return strMeasure7;
+        addMeasure(strMeasure6);
     }
 
     public void setStrMeasure7(String strMeasure7) {
-        this.strMeasure7 = strMeasure7;
-    }
-
-    public String getStrMeasure8() {
-        return strMeasure8;
+        addMeasure(strMeasure7);
     }
 
     public void setStrMeasure8(String strMeasure8) {
-        this.strMeasure8 = strMeasure8;
-    }
-
-    public String getStrMeasure9() {
-        return strMeasure9;
+        addMeasure(strMeasure8);
     }
 
     public void setStrMeasure9(String strMeasure9) {
-        this.strMeasure9 = strMeasure9;
-    }
-
-    public String getStrMeasure10() {
-        return strMeasure10;
+        addMeasure(strMeasure9);
     }
 
     public void setStrMeasure10(String strMeasure10) {
-        this.strMeasure10 = strMeasure10;
+        addMeasure(strMeasure10);
     }
 }
