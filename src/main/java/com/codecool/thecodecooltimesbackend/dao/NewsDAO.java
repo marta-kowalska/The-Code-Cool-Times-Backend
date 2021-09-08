abbba2dbd8e5ce54b3326ad14de50c75e965b84a
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-09-08 11:44:42 +0200

    [change] getTopNews return type to NewsResults

--- src/main/java/com/codecool/thecodecooltimesbackend/dao/NewsDAO.java
+++ src/main/java/com/codecool/thecodecooltimesbackend/dao/NewsDAO.java
@@ -1,9 +1,8 @@
 package com.codecool.thecodecooltimesbackend.dao;
 
-import com.codecool.thecodecooltimesbackend.model.News;
+import com.codecool.thecodecooltimesbackend.model.NewsResults;
 
-import java.util.List;
 
 public interface NewsDAO {
-    List<News> getTopNews();
+    NewsResults getTopNews();
 }
