ad3513a1758f460389d3409fc35611c80658561b
Zsolt Rokolya <rokyhu@gmail.com>
2021-09-09 09:48:56 +0200

    [add] context test for App

--- src/test/java/com/codecool/thecodecooltimesbackend/TheCodeCoolTimesBackendAppTests.java
+++ src/test/java/com/codecool/thecodecooltimesbackend/TheCodeCoolTimesBackendAppTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class TheCodeCoolTimesBackendAppTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }
