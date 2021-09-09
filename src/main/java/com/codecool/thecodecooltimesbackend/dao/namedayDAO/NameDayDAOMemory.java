371d4d9f09e5a460f9427c90ae7084bad1d171dc
Zsolt Rokolya <rokyhu@gmail.com>
2021-09-09 10:51:15 +0200

    [add] ApiRequester dependency to DAO Memory classes

--- src/main/java/com/codecool/thecodecooltimesbackend/dao/namedayDAO/NameDayDAOMemory.java
+++ src/main/java/com/codecool/thecodecooltimesbackend/dao/namedayDAO/NameDayDAOMemory.java
@@ -2,15 +2,23 @@ package com.codecool.thecodecooltimesbackend.dao.namedayDAO;
 
 import com.codecool.thecodecooltimesbackend.model.nameday.NameDayResult;
 import com.codecool.thecodecooltimesbackend.util.ApiRequester;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository("nameDayMemory")
 public class NameDayDAOMemory implements NameDayDAO {
 
+    private final ApiRequester apiRequester;
+
+    @Autowired
+    public NameDayDAOMemory(ApiRequester apiRequester) {
+        this.apiRequester = apiRequester;
+    }
+
     private static final String NAMEDAY_API = "https://nameday.abalin.net/today";
 
     @Override
     public NameDayResult getTodaysNameDay() {
-        return ApiRequester.fetchDataPost(NAMEDAY_API, NameDayResult.class);
+        return apiRequester.fetchDataPost(NAMEDAY_API, NameDayResult.class);
     }
 }
--- src/main/java/com/codecool/thecodecooltimesbackend/dao/quoteDAO/QuoteDAOMemory.java
+++ src/main/java/com/codecool/thecodecooltimesbackend/dao/quoteDAO/QuoteDAOMemory.java
@@ -3,20 +3,28 @@ package com.codecool.thecodecooltimesbackend.dao.quoteDAO;
 import com.codecool.thecodecooltimesbackend.config.AppProperties;
 import com.codecool.thecodecooltimesbackend.model.quote.Quote;
 import com.codecool.thecodecooltimesbackend.util.ApiRequester;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository("quoteMemory")
 public class QuoteDAOMemory implements QuoteDAO {
 
+    private final ApiRequester apiRequester;
+
     private Quote quote;
     private static final int MAX_QUOTE_LENGTH = 90;
+
+    @Autowired
+    public QuoteDAOMemory(ApiRequester apiRequester) {
+        this.apiRequester = apiRequester;
+    }
 
     public Quote getRandomQuote() {
         if (quote == null) {
             String quoteApiUrl = AppProperties.getQuoteApiUrl();
             int count = 0;
             do {
-                quote = ApiRequester.fetchDataGet(quoteApiUrl, Quote.class);
+                quote = apiRequester.fetchDataGet(quoteApiUrl, Quote.class);
                 System.out.println(++count);
             } while (quote.getQuote().length() > MAX_QUOTE_LENGTH);
         }
