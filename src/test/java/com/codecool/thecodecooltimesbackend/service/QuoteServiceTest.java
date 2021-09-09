528df4d95cd383ca983dbbb7e51c3013f0c9b631
Zsolt Rokolya <rokyhu@gmail.com>
2021-09-09 09:49:12 +0200

    [add] context test for Service Classes

---
+++ src/test/java/com/codecool/thecodecooltimesbackend/service/QuoteServiceTest.java
@@ -0,0 +1,23 @@
+package com.codecool.thecodecooltimesbackend.service;
+
+import com.codecool.thecodecooltimesbackend.dao.NewsDAO;
+import com.codecool.thecodecooltimesbackend.dao.quoteDAO.QuoteDAO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class QuoteServiceTest {
+
+    @Autowired
+    private QuoteDAO quoteDAO;
+
+    @Test
+    public void contextLoads() throws Exception {
+        assertThat(quoteDAO).isNotNull();
+    }
+
+}
