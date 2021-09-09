1b823b917000492ea2b17c679da489620eaf907d
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-09-09 10:27:59 +0200

    [add] WeatherResult

---
+++ src/main/java/com/codecool/thecodecooltimesbackend/model/weather/WeatherResult.java
@@ -0,0 +1,39 @@
+package com.codecool.thecodecooltimesbackend.model.weather;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+import java.util.Map;
+
+public class WeatherResult {
+
+    private static final String iconURL = "https://www.weatherbit.io/static/img/icons/{icon_code}.png";
+
+    private Double temperature;
+    private String description;
+    private String icon;
+
+
+    @SuppressWarnings("unchecked")
+    @JsonProperty("data")
+    public void unpackDetails(List<Object> data) {
+        Map<String,Object> details = (Map<String,Object>)data.get(0);
+        this.temperature = (Double) details.get("app_temp");
+        Map<String,Object> weather = (Map<String,Object>)details.get("weather");
+        this.description = (String) weather.get("description");
+        String iconCode = (String) weather.get("icon");
+        icon = iconURL.replace("{icon_code}", iconCode);
+    }
+
+    public Double getTemperature() {
+        return temperature;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+}
